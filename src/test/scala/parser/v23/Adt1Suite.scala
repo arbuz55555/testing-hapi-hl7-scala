141b54c91520ced666fda87eab85e004daadce89
arbuz55555 <arbuz55555@gmail.com>
2015-03-16 22:26:30 +0000

    copyright

--- src/test/scala/parser/v23/Adt1Suite.scala
+++ src/test/scala/parser/v23/Adt1Suite.scala
@@ -6,7 +6,21 @@ import ca.uhn.hl7v2.model.v23.message.ADT_A01
 import ca.uhn.hl7v2.parser.EncodingNotSupportedException
 import ca.uhn.hl7v2.{DefaultHapiContext, HL7Exception}
 import org.scalatest.FunSuite
-
+/**
+* Copyright 2011-2014 Lukasz Sztygiel 5x5 Solutions LTD (www.5x5sols.com)
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 class Adt1Suite extends FunSuite {
 
   test("ADT message V22 has ADT type A01 and Patient with familyName 'SMITH") {
